bbfea62b559a8302749185481bafffec5400a55c
Umut <omid.haqi@outlook.com>
2024-05-02 16:17:31 +0330

    init themes class

---
+++ lib/resources/themes/themes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Themes {
+  Themes._();
+  static ThemeData light = ThemeData(
+    brightness: Brightness.light,
+    fontFamily: 'Poppins'
+    
+  );
+  static ThemeData dark = ThemeData(
+    brightness: Brightness.dark,
+    fontFamily:'Poppins'
+
+  );
+}
