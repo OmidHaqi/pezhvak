fb4cbb4a6dc550409dcd1a37efb8556362b71bd4
Umut <omid.haqi@outlook.com>
2024-05-02 16:20:24 +0330

    init GetX and X509 Certificate

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,31 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pezhvak/my_http_overrides.dart';
+import 'package:pezhvak/resources/themes/themes.dart';
+import 'package:pezhvak/routes/named_route.dart';
+import 'package:pezhvak/routes/pages.dart';
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
+
+
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    
+    return GetMaterialApp(
       title: 'Pezhvak',
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Hello Pezhvak' )
-        ),
+      locale: const Locale('fa'),
+      debugShowCheckedModeBanner: false,
+      theme: Themes.light,
+      darkTheme: Themes.dark,
+      getPages: Pages.pages,
+      initialRoute: NamedRoute.initialRoute,
     );
   }
 }
