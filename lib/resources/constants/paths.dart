d187edb9a31a93bc0fd8b8ae0622c4276d29050a
Umut <omid.haqi@outlook.com>
2024-05-04 17:54:09 +0330

    add assets to paths

--- lib/resources/constants/paths.dart
+++ lib/resources/constants/paths.dart
@@ -1,6 +1,10 @@
 class Paths {
   Paths._();
 
-  static String splashLogo = 'assets/images/logo.png';
+  static String splashLogoPng = 'assets/images/logo.png';
+  static String dotPatternPng = 'assets/images/dot_pattern.png';
+  static String bgPng = 'assets/images/bg.png';
   static String splashLogoSvg = 'assets/svg/logo.svg';
+  static String dotPatternSvg = 'assets/svg/dot_pattern.svg';
+  static String bgSvg = 'assets/svg/bg.svg';
 }
