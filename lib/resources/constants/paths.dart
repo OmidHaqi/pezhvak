b2eb723a3785a9ae24004c94bb2bf539855fc0c1
Umut <omid.haqi@outlook.com>
2024-05-07 11:52:20 +0330

    add new assets and add in paths class

--- lib/resources/constants/paths.dart
+++ lib/resources/constants/paths.dart
@@ -7,4 +7,14 @@ class Paths {
   static String splashLogoSvg = 'assets/svg/logo.svg';
   static String dotPatternSvg = 'assets/svg/dot_pattern.svg';
   static String bgSvg = 'assets/svg/bg.svg';
+  static String icTelegram= 'assets/svg/ic_tel.svg';
+  static String icInstagram= 'assets/svg/ic_insta.svg';
+  static String icLinkdin= 'assets/svg/ic_linkdin.svg';
+  static String icGithub= 'assets/svg/ic_github.svg';
+  static String icYoutube= 'assets/svg/ic_youtube.svg';
+  static String icArrow= 'assets/svg/ic_arrow.svg';
+  static String vcCircle= 'assets/svg/vc_circle.svg';
+  static String vcPluse= 'assets/svg/vc_plus.svg';
+  static String vcTriangle= 'assets/svg/vc_triangle.svg';
+  static String vcVector= 'assets/svg/vc_vector.svg';
 }
