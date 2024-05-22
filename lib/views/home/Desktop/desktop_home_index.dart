ae4cc72ea17b3301ac9ab6a70b6b8568a9c005c8
Umut <omid.haqi@outlook.com>
2024-05-22 23:38:08 +0330

    create anime logic for shape

--- lib/views/home/Desktop/desktop_home_index.dart
+++ lib/views/home/Desktop/desktop_home_index.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 import 'package:pezhvak/resources/constants/paths.dart';
 
 class DesktopHomeIndex extends StatelessWidget {
-  const DesktopHomeIndex({
+  DesktopHomeIndex({
     super.key,
     required this.size,
   });
 
   final Size size;
+  final RxBool isHoverCircle = false.obs;
+  final RxBool isHoverPluse = false.obs;
+  final RxBool isHoverTriangle = false.obs;
+  final RxBool isHoverVector = false.obs;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: size.width,
-      height: size.height / 1.2,
+      height: size.height / 1.19,
       child: Padding(
         padding: const EdgeInsets.only(right: 50, top: 60),
         child: Row(
@@ -31,10 +35,10 @@ class DesktopHomeIndex extends StatelessWidget {
                 children: [
                   Padding(
                     padding: const EdgeInsets.only(right: 90),
-                    child: const Text(
+                    child: Text(
                       'گروه توسعه و طراحی پژواک',
                       style: TextStyle(
-                        fontSize: 64,
+                        fontSize: size.width / 27,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
@@ -47,15 +51,18 @@ class DesktopHomeIndex extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Padding(
-                        padding: const EdgeInsets.only(top: 10 , left: 10),
-                        child: SvgPicture.asset(Paths.vcCircle),
+                        padding: const EdgeInsets.only(top: 10, left: 10),
+                        child: SvgPicture.asset(
+                          Paths.vcCircle,
+                          width: size.width / 42,
+                        ),
                       ),
                       SizedBox(
                         width: size.width / 4,
-                        height: size.height/3,
-                        child: const Text(
+                        height: size.height / 3,
+                        child: Text(
                           'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز',
-                          style: TextStyle(fontSize: 22),
+                          style: TextStyle(fontSize: size.width / 78.5),
                           textAlign: TextAlign.justify,
                         ),
                       ),
@@ -64,8 +71,85 @@ class DesktopHomeIndex extends StatelessWidget {
                 ],
               ),
             ),
-             Expanded(
-              child: Image.asset(Paths.animationPng ,width:650,height: 650,))
+            Expanded(
+              child: Column(
+                children: [
+                  Stack(children: [
+                    Image.asset(
+                      Paths.animationPng,
+                      width: 650,
+                      height: 650,
+                    ),
+                    Positioned(
+                      top: 30,
+                      left: 50,
+                      child: Obx(
+                        () => InkWell(
+                          onTap: () {},
+                          onHover: (val) {
+                            isHoverCircle.value = val;
+                          },
+                          child: SvgPicture.asset(
+                            Paths.vcCircle,
+                            width: isHoverCircle.value
+                                ? size.width / 20
+                                : size.width / 30,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Obx(() => Positioned(
+                          left: 0,
+                          bottom: 250,
+                          child: InkWell(
+                            onTap: () {},
+                            onHover: (val) {
+                              isHoverPluse.value = val;
+                            },
+                            child: SvgPicture.asset(
+                              Paths.vcPluse,
+                              width: isHoverPluse.value
+                                  ? size.width / 45
+                                  : size.width / 55,
+                            ),
+                          ),
+                        )),
+                    Obx(() => Positioned(
+                          right: 50,
+                          bottom: 150,
+                          child: InkWell(
+                            onTap: () {},
+                            onHover: (val) {
+                              isHoverTriangle.value = val;
+                            },
+                            child: SvgPicture.asset(
+                              Paths.vcTriangle,
+                              width: isHoverTriangle.value
+                                  ? size.width / 24
+                                  : size.width / 34,
+                            ),
+                          ),
+                        )),
+                    Obx(() => Positioned(
+                          top: 10,
+                          right: 50,
+                          child: InkWell(
+                            onTap: () {},
+                            onHover: (val) {
+                              isHoverVector.value = val;
+                            },
+                            child: SvgPicture.asset(
+                              Paths.vcVector,
+                              width: isHoverVector.value
+                                  ? size.width / 27
+                                  : size.width / 37,
+                            ),
+                          ),
+                        )),
+                  ]),
+                ],
+              ),
+            )
           ],
         ),
       ),
